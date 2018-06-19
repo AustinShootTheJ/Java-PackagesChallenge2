8690d022e8329b23410b09f657921be49f41a54a
AustinShootTheJ <austinjmccune@gmail.com>
2018-06-19 16:12:50 -0400

    Initial commit

---
+++ src/com/example/pacakageschallenge2/Main.java
@@ -0,0 +1,12 @@
+package com.example.pacakageschallenge2;
+import com.example.pacakageschallenge.Series;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Series series = new Series();
+
+        System.out.println(series.nSum(10));
+
+    }
+}
